609eab0a9a9f8e33816194be1c9014df7390f5d4
Tim Brown <tbrown126@Tims-MacBook-Pro-2.local>
2013-09-15 14:31:29 -0700

    Now producing a tone of 440hz

--- timTestOut.py
+++ timTestOut.py
@@ -1,8 +1,11 @@
 import pyaudio, array, math
 
+RATE = 44100;
+HZ = 440;
+
 p = pyaudio.PyAudio()
 stream = p.open(rate=44100, channels=1, format=pyaudio.paFloat32, output=True)
 stream.write(array.array('f',
-    (.25 * math.sin(i / 10.) for i in range(44100))).tostring())
+    (math.sin(i*2*math.pi/(RATE/HZ)) for i in range(44100))).tostring())
 stream.close()
 p.terminate()
