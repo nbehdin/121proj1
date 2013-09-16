de251b16a3e94a85e9d57fcfc5045eecf73c2c58
Nader Behdin <naderbehdin@Nader-Behdins-MacBook.local>
2013-09-15 17:32:13 -0700

    Added decoding

--- readBitPattern.py
+++ readBitPattern.py
@@ -1,4 +1,5 @@
-import numpy, pyaudio, analyse
+import numpy, pyaudio, analyse, time
+from scipy.stats import mode
 
 RATE = 44100
 perSec = 16
@@ -17,7 +18,7 @@ def readAudio(dur):
         # Convert raw data to NumPy array
         samps = numpy.fromstring(rawsamps, dtype=numpy.int16)
         sampsArray.append(samps)
-    print sampsArray
+        #print sampsArray
     return sampsArray
 
 def postProcess(sampsArray):
@@ -26,7 +27,31 @@ def postProcess(sampsArray):
         frequencies.append(analyse.detect_pitch(samp))
     return frequencies   
 
+def decode(frequencies, samples):
+    print frequencies
+    count = 0
+    templist = []
+    finallist = []
+    for f in frequencies:
+        if count % samples == 0 and templist:
+           finallist.append(templist) 
+           templist = [f]
+        else:
+            templist.append(f)
+        count +=1
+    finallist.append(templist)
+
+    bits = []
+    for bit in finallist:
+        if mode(bit)[0][0] > 420 and mode(bit)[0][0] < 460:
+            bits.append(1)
+    print bits
+    
 if __name__ == '__main__':
+    start = time.time()
     samples = readAudio(1)
     frequencies = postProcess(samples)
+    print "Time: " , time.time() - start
+    #decode(frequencies, 4)
     print frequencies
+    
