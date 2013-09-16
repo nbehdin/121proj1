a1bc272f791de1438eb4ad3e1652c1f4a26bff3b
Tim Brown <tbrown126@Tims-MacBook-Pro-2.local>
2013-09-15 17:26:24 -0700

    Array is built before writing to our output stream

--- playBitPattern.py
+++ playBitPattern.py
@@ -2,14 +2,18 @@ import pyaudio, array, math, time
 
 RATE = 44100;
 
-def writeOneBit(bit, freq, dur, stream):
-        stream.write(array.array('f', (math.sin(bit*i*2*math.pi/(RATE/freq)) for i in range(int(RATE*dur)))).tostring())
-
+def writeOneBit(bit, freq, dur, toneArray):
+    toneArray.extend(array.array('f', (math.sin(bit*i*2*math.pi/(RATE/freq)) for i in range(int(RATE*dur)))))
+    
 def playBitStream(bits, freq, dur):
     p = pyaudio.PyAudio()
-    stream = p.open(rate=RATE, channels=1, format=pyaudio.paFloat32, output=True)
+    toneArray = array.array('f')
     for bit in bits:
-        writeOneBit(bit, freq, dur, stream)
+        writeOneBit(bit, freq, dur, toneArray)
+    start = time.time()
+    stream = p.open(rate=RATE, channels=1, format=pyaudio.paFloat32, output=True)
+    stream.write(toneArray.tostring())
+    print time.time() - start
     stream.close()
     p.terminate()
 
